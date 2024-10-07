a2257f886c7bfb10a26c2a912c06c993f69c0185
kdk1026 <kdk1026@gmail.com>
2024-10-07 21:47:20 +0900

    쉘 수정 (트랜잭션 대응)

--- shell/dev/shutdown.sh
+++ shell/dev/shutdown.sh
@@ -7,9 +7,19 @@ PROFILE=dev
 ## Rest Fixed
 pid=`ps -ef | grep -v grep | grep $WAR_FILE_NM | grep $PROFILE | awk '{print $2}'`
 
-if [ -n "$pid" ]
-then
-        kill -9 $(ps -ef | grep -v grep | grep $WAR_FILE_NM | grep $PROFILE | awk '{print $2}')
+if [ -n "$pid" ]; then
+	echo "Sending SIGTERM to process $pid"
+
+	kill -15 $pid
+
+	sleep 10
+
+	if ps -p $pid > /dev/null; then
+		echo "Process $pid did not terminate, sending SIGKILL"
+		kill -9 $pid
+	else
+		echo "Process $pid terminated successfully"
+	fi
 else
-        echo "PID is empty"
+	echo "PID is empty"
 fi
