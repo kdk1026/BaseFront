6c336285616515a340b0952e0af5aa80ac66d727
kdk1026 <kdk1026@gmail.com>
2025-01-27 21:19:28 +0900

    수정

--- src/main/java/com/kdk/config/mvc/HTMLCharacterEscapes.java
+++ src/main/java/com/kdk/config/mvc/HTMLCharacterEscapes.java
@@ -47,7 +47,16 @@ public class HTMLCharacterEscapes extends CharacterEscapes {
 	 */
 	@Override
 	public SerializableString getEscapeSequence(int ch) {
-		return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+		char charAt = (char) ch;
+		if ( Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt) ) {
+			// 이모지 대응
+			StringBuilder sb = new StringBuilder();
+	        sb.append("\\u");
+	        sb.append(String.format("%04x", ch));
+	        return new SerializedString(sb.toString());
+		} else {
+			return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+		}
 	}
 
 }
